d8832e48e3dd3a822c8933c63f47ff330ae06fdd
rasel-tm <rasel@technometrics.net>
2023-07-03 09:24:21 +0600

    disabled opening pdf inb browser

--- download_books_bengaliebooks.py
+++ download_books_bengaliebooks.py
@@ -24,6 +24,7 @@ class PDFDownloader:
         chrome_options.add_experimental_option('prefs', {
             "download.default_directory": self.download_directory,
             "download.prompt_for_download": False,
+            "plugins.always_open_pdf_externally": True
         })
         chrome_options.add_extension(f"{os.getcwd()}/ublock_origin.crx")
         self.driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
@@ -44,9 +45,22 @@ class PDFDownloader:
             seconds += 1
         return not dl_wait
 
+    def download_from_opened(self, file_count_old):
+        file_count = len(os.listdir(self.download_directory))
+        if file_count > file_count_old:
+            return False
+        else:
+            css_download_btn = '#download'
+            WebDriverWait(self.driver, self.delay).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, css_download_btn)))
+            download_btn = self.driver.find_element(By.CSS_SELECTOR, css_download_btn)
+            self.click_to_btn_js(self.driver, download_btn)
+            return True
+
     def get_book(self, url):
+        file_count = len(os.listdir(self.download_directory))
         self.driver.get(url)
-        css_download_btn = '.wp-block-button__link.has-background.wp-element-button'
+        css_download_btn = '.wp-block-button__link.has-background'
         WebDriverWait(self.driver, self.delay).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, css_download_btn)))
         download_btn = self.driver.find_element(By.CSS_SELECTOR, css_download_btn)
@@ -70,11 +84,12 @@ if __name__ == "__main__":
     urls = []
     with open(sys.argv[1]) as f:
         for line in f:
-            urls.append(line)
+            if line != '\n':
+                urls.append(line)
 
     report_file_failed_url = f'{DIR_REPORT}page_scraped_failed_url.txt'
     report_file_success_url = f'{DIR_REPORT}page_scraped_success_url.txt'
-
+    urls = set(urls)
     for url in urls:
         try:
             downloader.get_book(url)
